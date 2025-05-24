73a4869d565409d9678d67a07a490574ed62f8fc
Simon Gezahagn <simongezahagn@gmail.com>
2025-05-24 18:04:06 +0300

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// Declare the array of strings
+const messages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Use a loop to print each message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
